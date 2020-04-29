144d5f588f7fea7d760c0ed3ce1e04234d382f3a
张志强 <zhiqiang@bianjie.ai>
2020-04-29 17:39:54 +0800

    remove unused code

--- handler.go
+++ handler.go
@@ -85,19 +85,14 @@ func HandleMsgEditNFT(ctx sdk.Context, msg types.MsgEditNFT, k keeper.Keeper,
 // HandleMsgMintNFT handles MsgMintNFT
 func HandleMsgMintNFT(ctx sdk.Context, msg types.MsgMintNFT, k keeper.Keeper,
 ) (*sdk.Result, error) {
-	owner := msg.Recipient
-	if owner.Empty() {
-		owner = msg.Sender
-	}
-
-	if err := k.MintNFT(ctx, msg.Denom, msg.ID, msg.TokenURI, owner); err != nil {
+	if err := k.MintNFT(ctx, msg.Denom, msg.ID, msg.TokenURI, msg.Recipient); err != nil {
 		return nil, err
 	}
 
 	ctx.EventManager().EmitEvents(sdk.Events{
 		sdk.NewEvent(
 			types.EventTypeMintNFT,
-			sdk.NewAttribute(types.AttributeKeyRecipient, owner.String()),
+			sdk.NewAttribute(types.AttributeKeyRecipient, msg.Recipient.String()),
 			sdk.NewAttribute(types.AttributeKeyDenom, msg.Denom),
 			sdk.NewAttribute(types.AttributeKeyTokenID, msg.ID),
 			sdk.NewAttribute(types.AttributeKeyTokenURI, msg.TokenURI),
